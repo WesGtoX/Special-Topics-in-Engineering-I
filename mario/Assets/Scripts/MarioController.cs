fb6a3d5271c78f01b3b52badd3d8dfd5c91f33ec
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-06-17 22:35:20 -0300

    feat: mario jump and damage

--- mario/Assets/Scripts/MarioController.cs
+++ mario/Assets/Scripts/MarioController.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MarioController : MonoBehaviour {
     [SerializeField]
     float speedMax = 5.0f;
     [SerializeField]
+    float jumpImpulse = 7.0f;
+    [SerializeField]
+    float damageForce = 6f;
+    float damageTimeCounter = 0f;
+    float damageTime = 1f;
+    
+    int coinCounter = 0;
+    int life = 3;
+    
+    [SerializeField]
     bool isGrounded = true;
     bool isRunning = false;
     bool isJump = false;
     bool isCrouched = false;
+    bool isDamaged = false;
+
     [SerializeField]
-    float jumpImpulse = 7.0f;
+    VirtualButton jumpButton;
+    [SerializeField]
+    VirtualJoyStick joyStick;
+    
+    [SerializeField]
+    // Text coinCounterText;
     Animator animCtrl;
     SpriteRenderer spr;
     Rigidbody2D rb2d;
@@ -29,21 +47,27 @@ public class MarioController : MonoBehaviour {
     void Update() {
         CheckGround();
 
-        isJump = Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        // isJump = Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        isJump = jumpButton.GetButtonDown() || Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
         if (isJump && isGrounded == true)
             rb2d.AddForce(Vector2.up * jumpImpulse, ForceMode2D.Impulse);
 
         isCrouched = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
         if (!isCrouched)
             horizontalInput = Input.GetAxisRaw("Horizontal");
+            // horizontalInput = joyStick.GetHorizontal();
 
         isRunning = horizontalInput != 0 && Input.GetKey(KeyCode.LeftShift);
         UpdateAnimation();
+
+        CheckDamagedState();
     }
 
     void FixedUpdate() {
-        float speedRun = isRunning ? 1.5f : 1f;
-        rb2d.velocity = new Vector2(horizontalInput * (speedMax * speedRun), rb2d.velocity.y);
+        if (!isDamaged) {
+            float speedRun = isRunning ? 1.5f : 1f;
+            rb2d.velocity = new Vector2(horizontalInput * (speedMax * speedRun), rb2d.velocity.y);
+        }
     }
 
     void UpdateAnimation() {
@@ -53,7 +77,7 @@ public class MarioController : MonoBehaviour {
         animCtrl.SetBool("isCrouched", isCrouched);
 
         if (horizontalInput != 0)
-            animCtrl.SetFloat("direction", horizontalInput);           
+            animCtrl.SetFloat("direction", horizontalInput);
 
         // Apenas para o caso de utilização de animações para a direita
         // if (horizontalInput > 0) {
@@ -67,5 +91,43 @@ public class MarioController : MonoBehaviour {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
         isGrounded = hit.collider != null;
         // Debug.DrawRay(transform.position, Vector2.down * 0.1f, Color.red);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("coin")) {
+            coinCounter++;
+            Destroy(collision.gameObject);
+            // coinCounterText.text = coinCounter.ToString();
+        } else if (collision.CompareTag("enemy")) {
+            float marioPosX = transform.position.x;
+            float enemyPosX = collision.transform.position.x;
+            bool isLeft = (marioPosX >= enemyPosX);
+            TakeDamage(isLeft);
+        }
+    }
+
+    void TakeDamage(bool isLeft) {
+        isDamaged = true;
+        
+        Vector2 directionForce = isLeft ?new Vector2(1f, 1f) : new Vector2(-1f, 1f);
+        rb2d.velocity = new Vector2(0f, 0f);
+        rb2d.AddForce(directionForce * damageForce, ForceMode2D.Impulse);
+
+        life--;
+        if (life < 0) {
+            UnityEditor.EditorApplication.isPlaying = false;
+            // Application.Quit();
+        }
+    }
+
+    void CheckDamagedState() {
+        if (isDamaged) {
+            damageTimeCounter += Time.deltaTime;
+            
+            if (damageTimeCounter >= damageTime) {
+                isDamaged = false;
+                damageTimeCounter = 0f;
+            }
+        }
     }
 }
