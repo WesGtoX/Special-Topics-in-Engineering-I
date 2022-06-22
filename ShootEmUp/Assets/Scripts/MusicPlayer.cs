78d4beb07a2073486410e33a68ac9b9e818f6406
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-06-22 00:08:20 -0300

    feat: adding background music

---
+++ ShootEmUp/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    public AudioClip[] songs;
+
+    private AudioSource audioSource;
+    private int index;
+    private static MusicPlayer musicPlayer;
+
+    private void Start() {
+        index = Random.Range(0, songs.Length);
+        StartMusicPlayer();
+    }
+
+    void Awake() {
+        if (musicPlayer == null) {
+            musicPlayer = this;
+        } else if (musicPlayer != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void StartMusicPlayer() {
+        audioSource.clip = songs[index];
+        index++;
+        
+        if (index > songs.Length) {
+            index = 0;
+        }
+
+        audioSource.Play();
+        Invoke("StartMusicPlayer", audioSource.clip.length + 0.5f);
+    }
+}
